e996e6aa9779e7854a3bb8c5efaf49838a0ec92c
Kumar Rishabh <kr666rishabh@gmail.com>
2022-10-03 09:17:30 +0530

    Remove redundant lines, update typingData

--- auto-typing.py
+++ auto-typing.py
@@ -4,7 +4,5 @@ import sys
 import os
 
 time.sleep(30)
-# for line in open("typingData.txt", "r"):
 for line in open(os.path.join(sys.path[0], 'typingData.txt')):
     pyautogui.typewrite(line)
-    # pyautogui.press("enter")
